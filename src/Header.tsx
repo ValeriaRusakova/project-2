949957efd5cc00722e59984aa7d9fff5de4bf5b7
ValeriaRusakova <rus.valeria777@gmail.com>
2026-03-07 01:15:54 +0200

    commit after backround image

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,9 @@
 // קומפוננטת Header - כותרת עם אפקט Parallax
+import headerBg from './assets/generated-image.png';
+
 function Header() {
   return (
-    <header className="header">
+    <header className="header" style={{ backgroundImage: `linear-gradient(rgba(33, 150, 243, 0.7), rgba(21, 101, 192, 0.75)), url(${headerBg})` }}>
       <div className="header-content">
         <h1 className="header-title">Cryptonite</h1>
       </div>
