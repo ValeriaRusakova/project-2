8e0a544811ed8199e39ad36d79e73fc98d3fc2a9
ValeriaRusakova <rus.valeria777@gmail.com>
2026-03-07 13:20:06 +0200

    commit with better photo

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import headerBg from './assets/generated-image.png';
 
 function Header() {
   return (
-    <header className="header" style={{ backgroundImage: `linear-gradient(rgba(33, 150, 243, 0.7), rgba(21, 101, 192, 0.75)), url(${headerBg})` }}>
+    <header className="header" style={{ backgroundImage: `url(${headerBg})` }}>
       <div className="header-content">
         <h1 className="header-title">Cryptonite</h1>
       </div>
